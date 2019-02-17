41031a3999547edf4bab3418668f46eab94cb393
Victor Pontis <victor@pont.is>
2019-02-17 13:20:08 -0800

    Add deploy script

--- next.config.js
+++ next.config.js
@@ -2,4 +2,10 @@
 const withTypescript = require('@zeit/next-typescript')
 const withCSS = require('@zeit/next-css')
 
-module.exports = withTypescript(withCSS())
+module.exports = withTypescript(withCSS({
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' }
+    }
+  },
+}))
