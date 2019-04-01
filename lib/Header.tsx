5dbaa6dbbd23fe42e17cc98c967800f22a05f144
Victor Pontis <victor@pont.is>
2019-04-01 13:01:28 -0700

    Add lighthaus favicon

--- lib/Header.tsx
+++ lib/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => (
   <Head>
     <title>Lighthaus</title>
     <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-    <link rel="shortcut icon" href="/static/favicon.ico"/>
+    <link rel="shortcut icon" href="/static/lighthaus-favicon.png"/>
   </Head>
 )
 
